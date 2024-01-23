61c31ebd274c2852336a604337206dc9bd22a268
aranciata1 <157031630+aranciata1@users.noreply.github.com>
2024-01-23 09:46:19 -0500

    class notes

---
+++ notes/classnotes01_23.R
@@ -0,0 +1,8 @@
+## viz.ev.market.share.data.r
+# in class notes T Jan 23
+
+df = data.frame(year = 2015:2023,
+                ev = c(0.3, 0.4, 0.5, 1.2, 1.3, 1.6, 2.6, 5.2, 6.9),
+                hev = c(1.8, 2, 2.1, 2.5, 3.5, 4, 5, 5.5, 8.3))
+
+
